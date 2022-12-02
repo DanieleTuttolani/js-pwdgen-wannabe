b8ffc7897dec5ca6327dba05f00a4808f17fb63a
DanieleTuttolani <danieletuttolani2@gmail.com>
2022-12-02 15:18:03 +0100

    dati utente

--- .script/script.js
+++ .script/script.js
@@ -2,6 +2,13 @@ console.log("js ok");
 
 
 
-//prendo il  DOC
+//prendo il  DOC***************
 const placeholder= document.getElementById("target");
 //console.log(placeholder);
+
+//chiedo all'utenza i dati****************
+ const username= prompt("inserisci il tuo nome", "daniele");
+ const usersurname= prompt("inserisci il tuo cognome", "tuttolani");
+ const usercolor= prompt("inserisci il tuo colore preferito", "arancione");
+
+ //console.log(usercolor);
